79302c0a6bd985cd2ac2ab8c54f5e99dcde8f66f
dubovska.alla <alla.dubovska@gmail.com>
2017-06-20 13:02:11 +0300

    #7[1h]. Sending email with error.

---
+++ src/email/EmailManager.java
@@ -0,0 +1,59 @@
+package email;
+
+import settings.Settings;
+
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * The main purpose of this class is to send email in case smth went wrong during fetching or parsing rss feeds.
+ * <p>
+ * Created by alla on 6/20/17.
+ */
+public class EmailManager {
+
+    private static final String username = "scraper.idsm2016@gmail.com";
+    private static final String password = "qwaszx123";
+
+    private static EmailManager instance = new EmailManager();
+
+    private static Properties props;
+
+    public static EmailManager get() {
+        return instance;
+    }
+
+    private EmailManager() {
+        props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+    }
+
+    public static void send(String problem, String rss_link_name) {
+        if (Settings.get().getEmail() == null) return;
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(Settings.get().getEmail()));
+            message.setSubject("Opps, smth went wrong during fetching or parsing rss feeds");
+            message.setText("Dear " + (Settings.get().getUsername() == null ? "user" : Settings.get().getUsername()) +
+                    ",\n\nThe problem appeared during the fetching of " + rss_link_name + " feed. \n\n " +
+                    problem + "\n\nBest regards");
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
